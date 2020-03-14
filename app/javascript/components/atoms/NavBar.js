472903f4b942aea704ab8f7b879afd2191f98385
Simon Fish <si@mon.fish>
2020-03-14 14:42:29 +0000

    Create initial navbar with login button
    Todo: button changes if logged in, perhaps make it ajax?
    
    session.userinfo doesn't seem to persist. I'll need to fix that for it
    to work.

--- app/javascript/components/atoms/NavBar.js
+++ app/javascript/components/atoms/NavBar.js
@@ -1,5 +1,60 @@
-import React from "react"
+import React, { useState } from "react"
 
-export default ({ currentPage }) => (
-  <div>CurrentPage: {currentPage}</div>
-)
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+  Button
+} from 'reactstrap';
+
+export default ({ currentPage, loginButton }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Blacklight</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/feed">Feed</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/boardfish/blacklight">GitHub</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Option 1
+                </DropdownItem>
+                <DropdownItem>
+                  Option 2
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <div dangerouslySetInnerHTML={{ __html: loginButton }} />
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
