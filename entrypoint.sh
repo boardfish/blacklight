c2f86e4182360dc17282a341df32c4f6e06afa05
Simon Fish <si@mon.fish>
2020-03-20 10:44:38 +0000

    Add and use binstubs for rspec and rails

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ set -e
 rm -f /blacklight/tmp/pids/server.pid
 
 bundle check || bundle install
+bundle binstubs rspec-core rails
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
