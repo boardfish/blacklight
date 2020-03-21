e5211296c466f38fe26bb9d9a7efc5b13eb44081
Simon Fish <si@mon.fish>
2020-03-21 15:57:28 +0000

    Make Rubocop happy

--- spec/controllers/images_controller_spec.rb
+++ spec/controllers/images_controller_spec.rb
@@ -16,7 +16,9 @@ RSpec.describe ImagesController, type: :controller do
     puts @escape_game.inspect
     puts @escape_game.images.last
     expect do
-      delete :destroy, params: { escape_game_id: @escape_game.id, id: @escape_game.images.last.id }
+      delete :destroy, params: {
+        escape_game_id: @escape_game.id, id: @escape_game.images.last.id
+      }
     end.to change { @escape_game.images.count }.by(-1)
   end
 end
--- spec/requests/images_request_spec.rb
+++ spec/requests/images_request_spec.rb
@@ -3,10 +3,4 @@
 require 'rails_helper'
 
 RSpec.describe 'Images', type: :request do
-  describe 'DELETE /escape_games/1/images/1' do
-    it 'returns http success' do
-      delete escape_game_image_path(1, 1)
-      expect(response).to have_http_status(:success)
-    end
-  end
 end
