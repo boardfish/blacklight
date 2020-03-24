84a0f57d74a5a12eb882f5039c9c1deb7b3a53ee
Simon Fish <si@mon.fish>
2020-03-24 14:33:46 +0000

    Add test that Auth0 auth route creates users from the auth hash

--- spec/controllers/users/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/users/omniauth_callbacks_controller_spec.rb
@@ -3,4 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe Users::OmniauthCallbacksController, type: :controller do
+
+  before(:each) do
+    request.env["devise.mapping"] = Devise.mappings[:user] # If using Devise
+    request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:auth0] 
+  end
+
+  it 'creates a user through Auth0' do
+    expect { post :auth0 } .to change(User, :count).by(1)
+    expect(User.last.uid).to eq('1')
+    expect(User.last.name).to eq('Foo Bar')
+    expect(User.last.nickname).to eq('foo')
+    expect(User.last.email).to eq('foo@bar.com')
+  end
 end
