4cc32d2d1a278fcd4c566a76ca2c6f7bf49f7b8b
Simon Fish <si@mon.fish>
2020-04-06 10:05:59 +0100

    Move metadata and clear button out of image

--- app/javascript/components/molecules/EscapeGame.js
+++ app/javascript/components/molecules/EscapeGame.js
@@ -4,7 +4,7 @@ import { Card, CardBody, CardHeader, CardFooter, CardImg } from "reactstrap";
 import BlurHashTest from "../atoms/BlurHashTest";
 import EscapeGameMetadata from "../atoms/EscapeGameMetadata";
 
-const chooseColor = exploring => {
+const chooseColor = (exploring) => {
   switch (exploring) {
     case false:
       return ["secondary", "success"];
@@ -20,24 +20,45 @@ export default ({
   escapeGame,
   imagePath,
   authenticity_token,
-  exploring
+  exploring,
 }) => {
-  const [hovered, setHovered] = useState('');
+  const [hovered, setHovered] = useState("");
   return (
     <a
-      onMouseOver={() => setHovered('focused')}
-      onMouseOut={() => setHovered('unfocused')}
-      onFocus={() => setHovered('focused')}
-      onBlur={() => setHovered('unfocused')}
+      onMouseOver={() => setHovered("focused")}
+      onMouseOut={() => setHovered("unfocused")}
+      onFocus={() => setHovered("focused")}
+      onBlur={() => setHovered("unfocused")}
       href={`/escape_games/${escapeGame.id}`}
       className={`card ${hovered}`}
       tabIndex={1}
-      >
+    >
       <CardHeader>
         <div className="d-flex flex-row">
           <h4 className="escape-game-name">{escapeGame.name}</h4>
         </div>
         <p className="escape-game-summary">{escapeGame.summary}</p>
+        <h5 className="d-flex justify-content-end align-items-center">
+          <ClearButton
+            cleared={cleared}
+            size="sm"
+            stateColors={chooseColor(exploring, cleared)}
+            escapeGameId={escapeGame.id}
+            authenticity_token={authenticity_token}
+            className="mr-auto"
+          />
+          <EscapeGameMetadata
+            id={escapeGame.id}
+            difficultyLevel={escapeGame.difficulty_level}
+            availableTime={escapeGame.available_time}
+            genre={escapeGame.genre}
+            location={{
+              latitude: escapeGame.latitude,
+              longitude: escapeGame.longitude,
+              placeId: escapeGame.place_id,
+            }}
+          />
+        </h5>
       </CardHeader>
       {imagePath ? (
         <BlurHashTest
@@ -48,61 +69,12 @@ export default ({
             width: "100%",
             height: "200px",
             resolutionX: 350,
-            punch: 1
+            punch: 1,
           }}
-        >
-          <ClearButton
-            cleared={cleared}
-            size="sm"
-            stateColors={chooseColor(exploring, cleared)}
-            escapeGameId={escapeGame.id}
-            authenticity_token={authenticity_token}
-            style={{ zIndex: 1 }}
-          />
-          <h5 className="ml-auto mt-auto mr-2" style={{ zIndex: 1 }}>
-            <EscapeGameMetadata
-              id={escapeGame.id}
-              difficultyLevel={escapeGame.difficulty_level}
-              availableTime={escapeGame.available_time}
-              genre={escapeGame.genre}
-              location={{
-                latitude: escapeGame.latitude,
-                longitude: escapeGame.longitude,
-                placeId: escapeGame.place_id
-              }}
-            />
-          </h5>
-        </BlurHashTest>
+        />
       ) : (
         ""
       )}
-      <CardBody>
-        {!imagePath ? (
-          <h5 className="d-flex justify-content-end align-items-center">
-            <ClearButton
-              cleared={cleared}
-              size="sm"
-              stateColors={chooseColor(exploring, cleared)}
-              escapeGameId={escapeGame.id}
-              authenticity_token={authenticity_token}
-              className="mr-auto"
-            />
-            <EscapeGameMetadata
-              id={escapeGame.id}
-              difficultyLevel={escapeGame.difficulty_level}
-              availableTime={escapeGame.available_time}
-              genre={escapeGame.genre}
-              location={{
-                latitude: escapeGame.latitude,
-                longitude: escapeGame.longitude,
-                placeId: escapeGame.place_id
-              }}
-            />
-          </h5>
-        ) : (
-          ""
-        )}
-      </CardBody>
     </a>
   );
 };
