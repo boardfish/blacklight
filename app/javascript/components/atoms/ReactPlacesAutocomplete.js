831c5a4943472fffbb60bc5ee61fec7f8ca204e3
Simon Fish <si@mon.fish>
2020-04-07 10:25:02 +0100

    Use react places autocomplete to update field
    Now I just need to style it to look more Bootstrap.

--- app/javascript/components/atoms/ReactPlacesAutocomplete.js
+++ app/javascript/components/atoms/ReactPlacesAutocomplete.js
@@ -1,56 +1,81 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import PlacesAutocomplete, {
   geocodeByAddress,
   getLatLng,
 } from "react-places-autocomplete";
 
 export default () => {
-  const [address, setAddress] = useState('');
+  const [address, setAddress] = useState("");
+  const [selectedPlace, setSelectedPlace] = useState({
+    lat: "",
+    lng: "",
+    placeId: "",
+  });
 
-  const handleSelect = (address) => {
+  const handleSelect = (address, placeId) => {
     geocodeByAddress(address)
       .then((results) => getLatLng(results[0]))
-      .then((latLng) => console.log("Success", latLng))
+      .then((latLng) => setSelectedPlace({ ...latLng, placeId }))
       .catch((error) => console.error("Error", error));
   };
   return (
-    <PlacesAutocomplete
-      value={address}
-      onChange={(newAddress) => setAddress(newAddress)}
-      onSelect={handleSelect}
-    >
-      {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-        <div>
-          <input
-            {...getInputProps({
-              placeholder: "Search Places ...",
-              className: "location-search-input",
-            })}
-          />
-          <div className="autocomplete-dropdown-container">
-            {loading && <div>Loading...</div>}
-            {suggestions.map((suggestion) => {
-              const className = suggestion.active
-                ? "suggestion-item--active"
-                : "suggestion-item";
-              // inline style for demonstration purpose
-              const style = suggestion.active
-                ? { backgroundColor: "#fafafa", cursor: "pointer" }
-                : { backgroundColor: "#ffffff", cursor: "pointer" };
-              return (
-                <div
-                  {...getSuggestionItemProps(suggestion, {
-                    className,
-                    style,
-                  })}
-                >
-                  <span>{suggestion.description}</span>
-                </div>
-              );
-            })}
+    <Fragment>
+      <input
+        type="hidden"
+        name="escape_game[latitude]"
+        value={selectedPlace.lat}
+        readOnly
+      />
+      <input
+        type="hidden"
+        name="escape_game[longitude]"
+        value={selectedPlace.lng}
+        readOnly
+      />
+      <input
+        type="hidden"
+        name="escape_game[place_id]"
+        value={selectedPlace.placeId}
+        readOnly
+      />
+      <PlacesAutocomplete
+        value={address}
+        onChange={(newAddress) => setAddress(newAddress)}
+        onSelect={handleSelect}
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div>
+            <input
+              {...getInputProps({
+                placeholder: "Search Places ...",
+                className: "location-search-input",
+              })}
+            />
+            <div className="autocomplete-dropdown-container">
+              {loading && <div>Loading...</div>}
+              {suggestions.map((suggestion) => {
+                const className = suggestion.active
+                  ? "suggestion-item--active"
+                  : "suggestion-item";
+                // inline style for demonstration purpose
+                const style = suggestion.active
+                  ? { backgroundColor: "#fafafa", cursor: "pointer" }
+                  : { backgroundColor: "#ffffff", cursor: "pointer" };
+                return (
+                  <div
+                    {...getSuggestionItemProps(suggestion, {
+                      className,
+                      style,
+                    })}
+                  >
+                    <span>{suggestion.description}</span>
+                  </div>
+                );
+              })}
+            </div>
           </div>
-        </div>
-      )}
-    </PlacesAutocomplete>
+        )}
+      </PlacesAutocomplete>
+    </Fragment>
   );
 };
