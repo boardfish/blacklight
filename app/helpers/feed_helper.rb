09c057760232d12c22e28681124d52ed986904a3
Simon Fish <si@mon.fish>
2020-04-08 16:27:42 +0100

    Remove feed
    This feature has been abandoned due to time constraints

--- app/helpers/feed_helper.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-# Helper class for views under the Feed controller.
-module FeedHelper
-end
--- config/routes.rb
+++ config/routes.rb
@@ -12,15 +12,12 @@ Rails.application.routes.draw do
   resources :users, except: :index
   get 'sessions/new'
   get 'sessions/destroy'
-  get 'feed/show'
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "home#index"
 
   # get 'auth/auth0/callback' => 'auth0#callback'
   # get 'auth/failure' => 'auth0#failure'
-
-  get 'feed' => 'feed#show'
 
   devise_for :users, skip: [:sessions], :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   devise_scope :user do
