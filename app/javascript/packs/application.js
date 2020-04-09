c9fee4e71d81848291cb3714cc5091e81223a4cd
Simon Fish <si@mon.fish>
2020-04-09 11:20:07 +0100

    Use local Bootstrap, jQuery, Popper

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,6 +18,8 @@ require('../iconInit')
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 // Support component names relative to this directory:
+import 'bootstrap'
+
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
