9535d2008682de264703bcff0658a2247bd4fae9
Simon Fish <si@mon.fish>
2020-04-09 16:27:43 +0100

    Seed a couple of maintainers, give enthusiasts random clears

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,23 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'factory_bot_rails' 
 
-seed_user = FactoryBot.create(:user)
-100.times do
-  FactoryBot.create(:escape_game, 
-    user: seed_user
-  )
+data = YAML.load_file(Rails.root.join('spec/fixtures/files/escape_game/data.yml')).deep_symbolize_keys
+data[:users].each do |user|
+  user_record = FactoryBot.create(:user, **user.except(:escape_games, :avatar))
+  user[:escape_games].each do |eg_data|
+    FactoryBot.create(:escape_game, **eg_data.except(:latitude, :longitude, :place_id, :images), user: user_record)
+  end
 end
+User.where(enthusiast: true).each do |user|
+  EscapeGame.order("RANDOM()").take(rand(EscapeGame.count - 1) + 1).each do |escape_game|
+    Clear.create(user: user, escape_game: escape_game)
+  end
+end
+# seed_user = FactoryBot.create(:user)
+# 100.times do
+#   FactoryBot.create(:escape_game, 
+#     user: seed_user
+#   )
+# end
 # Build blurhashes for newly attached images
 EscapeGame.find_each(&:save)
