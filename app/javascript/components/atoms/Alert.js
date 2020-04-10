d812f571fff523055ea83c615eb2368a0a61aca5
Simon Fish <si@mon.fish>
2020-04-10 11:42:14 +0100

    Clean up alert visual style

--- app/javascript/components/atoms/Alert.js
+++ app/javascript/components/atoms/Alert.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { UncontrolledAlert } from 'reactstrap';
 
 export default ({ type, alert }) => ( 
-  <UncontrolledAlert color={type}>
+  <UncontrolledAlert color={type} className="m-3">
     <h4 className="alert-heading">{alert.title}</h4>
     <p className="mb-0">{alert.content}</p>
   </UncontrolledAlert>
