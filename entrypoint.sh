a010dc40e563f7dbe30de39ef6c83e0eb3d0af39
Simon Fish <si@mon.fish>
2020-05-12 11:59:11 +0100

    Precompile assets
    These weren't loading on first run.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ rm -f /blacklight/tmp/pids/server.pid
 
 bundle check || bundle install
 bundle binstubs rspec-core rails
+bin/rails assets:precompile
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
